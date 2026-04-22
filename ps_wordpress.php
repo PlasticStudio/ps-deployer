fcff871f30231594d1680a8639b91a0de2b40da9
brad-ps <107819630+brad-ps@users.noreply.github.com>
2026-04-22 16:24:31 +1200

    Update ps_wordpress.php

--- ps_wordpress.php
+++ ps_wordpress.php
@@ -2,6 +2,8 @@
 
 namespace Deployer;
 
+use Deployer\Exception\GracefulShutdownException;
+
 set('keep_releases', 5);
 set('writable_mode', 'chmod');
 set('remote_db_backup_path', '/container/backups/containers/latest/databases/');
@@ -9,6 +11,7 @@ set('deploy_path', '/container/application/theme');
 set('identity_file', '~/.ssh/id_rsa');
 set('shared_path', '/container/application/shared');
 set('sitehost_restart_mode', 'container');
+set('sub_directory', 'wp-content/themes');
 
 set('shared_dirs', []);
 set('shared_files', []);
@@ -25,6 +28,59 @@ task('wordpress:theme:symlink', function () {
 
     run('ln -sfn ' . $deployerCurrent . ' ' . $wpThemeDir);
     writeln('<info>Symlinked: ' . $wpThemeDir . ' → ' . $deployerCurrent . '</info>');
+});
+
+task('confirm', function () {
+    if (!askConfirmation('Are you sure you want to deploy to production?')) {
+        writeln('Ok, quitting.');
+        throw new GracefulShutdownException('User aborted the deployment.');
+    }
+})->select('stage=prod');
+
+// Sitehost tasks (copied from ps_silverstripe.php, only what WP needs)
+task('sitehost:restart', function () {
+    if (get('sitehost_restart_mode') == 'apache-php') {
+        writeln('<info>Restarting apache & php</info>');
+        run('supervisorctl restart apache2 php');
+        return;
+    }
+
+    if (testLocally('[ -f /var/www/sitehost-api-key.txt ]')) {
+        $config = file_get_contents('/var/www/sitehost-api-key.txt');
+        set('sitehost_api_key', trim($config));
+    }
+
+    if (!get('sitehost_api_key')) {
+        writeln('<error>SKIPPING SITEHOST RESTART - sitehost_api_key not set</error>');
+        return;
+    }
+    if (!get('sitehost_client_id')) {
+        writeln('<error>SKIPPING SITEHOST RESTART - sitehost_client_id not set</error>');
+        return;
+    }
+    if (!get('sitehost_server_name')) {
+        writeln('<error>SKIPPING SITEHOST RESTART - sitehost_server_name not set</error>');
+        return;
+    }
+    if (!get('sitehost_stack_name')) {
+        writeln('<error>SKIPPING SITEHOST RESTART - sitehost_stack_name not set</error>');
+        return;
+    }
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://api.sitehost.nz/1.2/cloud/stack/restart.json");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, [
+        'apikey'    => get('sitehost_api_key'),
+        'client_id' => get('sitehost_client_id'),
+        'server'    => get('sitehost_server_name'),
+        'name'      => get('sitehost_stack_name'),
+    ]);
+    writeln('<info>Triggering container restart {{sitehost_stack_name}} on {{sitehost_server_name}}</info>');
+    $response = curl_exec($ch);
+    writeln('<info>Response from Sitehost: ' . $response . '</info>');
+    curl_close($ch);
 });
 
 desc('Deploy theme');
